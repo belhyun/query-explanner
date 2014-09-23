e000b24b83b938d761852515b41e4497f139b574
belhyun <belhyun@gmail.com>
2014-09-23 13:07:02 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QueryExplanner::Application.config.secret_key_base = '30454e6a63378e5ae7866b51a4c58f69456d8c17b8cbec695a971524aee87aab974f034dd83f41529309783dbb42743ff48b243d4fa3accc1f33578c66318492'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QueryExplanner::Application.config.session_store :cookie_store, key: '_query-explanner_session'
